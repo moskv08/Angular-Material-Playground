a6e2b6e89c91d4ba3d8a0e2d106851f153485845
robbmoskv <fabrizio_39@hotmail.com>
2020-05-13 20:59:40 +0200

    add flexbox component routing

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DemoRoutingModule } from './demo-routing.module';
-import { ButtonsComponent } from './buttons/buttons.component';
-
-import { MaterialModule } from '../shared/material.module';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule} from '@angular/flex-layout';
+
+import { DemoRoutingModule } from './demo-routing.module';
+import { MaterialModule } from '../shared/material.module';
+
 import { FlexboxComponent } from './flexbox/flexbox.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 
 @NgModule({
@@ -15,6 +17,7 @@ import { FlexboxComponent } from './flexbox/flexbox.component';
     CommonModule,
     DemoRoutingModule,
     MaterialModule,
+    FlexLayoutModule,
     FormsModule,
   ]
 })
