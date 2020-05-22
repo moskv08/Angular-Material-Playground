900bebb5e24283bb2bc162d4a8e35c157d92e215
robbmoskv <fabrizio_39@hotmail.com>
2020-05-22 16:23:52 +0200

    create behavior subject and make it available

--- src/app/contactmanager/services/user.service.ts
+++ src/app/contactmanager/services/user.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
+  private _users: BehaviorSubject<User[]>;
 
   private dataStore: {
     users: User[];
@@ -14,6 +17,12 @@ export class UserService {
   constructor(private http: HttpClient) {
 
     this.dataStore = { users: [] };
+    this._users = new BehaviorSubject<User[]>([]);
+  }
+
+  // Allow components to subscribe to behavior subject
+  get users(): Observable<User[]> {
+    return this._users.asObservable();
   }
   
   loadAllUsers() {
@@ -22,10 +31,12 @@ export class UserService {
     this.http.get<User[]>(url).subscribe(
       data => {
         this.dataStore.users = data;
+        // inform subscribers about changes
+        this._users.next(Object.assign({}, this.dataStore).users);
       }, error => {
         console.log(`Failed to fetch this thing. Error: ${error.message}`);
       }
-    )
+    );
 
   }
 }
