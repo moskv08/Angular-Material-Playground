0216ff8b21bb09c3ed7fc03e64a47e5de4ca2dfe
robbmoskv <fabrizio_39@hotmail.com>
2020-05-30 23:35:41 +0200

    Place a new Toolbar menu

--- src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ContactDialogComponent } from '../../contact-dialog/contact-dialog.component';
 
 @Component({
   selector: 'app-toolbar',
@@ -8,9 +10,19 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class ToolbarComponent implements OnInit {
 
   @Output() toggleSidenav = new EventEmitter<void>();
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  openNewContactDialog(): void {
+    let dialogRef = this.dialog.open(ContactDialogComponent, { 
+      width: '450px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('Dialog was closed.', result);
+    });
   }
 
 }
