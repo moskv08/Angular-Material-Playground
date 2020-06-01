7207f87f7aa1263836bff8bf108bd2b5b6a5a9a1
robbmoskv <fabrizio_39@hotmail.com>
2020-06-01 21:31:50 +0200

    Create a snackbar method on create new users

--- src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ContactDialogComponent } from '../../components/contact-dialog/contact-dialog.component';
+import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,10 +12,9 @@ import { ContactDialogComponent } from '../../components/contact-dialog/contact-
 export class ToolbarComponent implements OnInit {
 
   @Output() toggleSidenav = new EventEmitter<void>();
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private snackBar: MatSnackBar, private router: Router) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   openNewContactDialog(): void {
     let dialogRef = this.dialog.open(ContactDialogComponent, { 
@@ -22,6 +23,20 @@ export class ToolbarComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('Dialog was closed.', result);
+
+      // No NULL was passed back (close with a save action)
+      if (result) {
+        this.openSnackBar("Contact added.", "Navigate").onAction().subscribe(() => {
+          // Navigate to new created user
+          this.router.navigate(['/contactmanager', result.id]);
+        });
+      }
+    });
+  }
+
+  openSnackBar(message: string, action: string): MatSnackBarRef<SimpleSnackBar> {
+    return this.snackBar.open(message, action, {
+      duration: 5000,
     });
   }
 
